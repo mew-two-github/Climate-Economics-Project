d086a342b5d5140399813269c2277beb821f8b07
vishal <subhavishal@yahoo.co.in>
2021-10-18 11:28:38 +0530

    arima_remarks working

--- utils.m
+++ utils.m
@@ -1,9 +1,17 @@
+%% Main function
 function [f1,f2] = utils()
     f1 = @arima_remarks;
+    f2 = @chowt;
 end
-function [est_m,res,underfit,overfit] = arima_remarks(params,vk)
+%% arima_remarks
+% Performs arima modelling for given parameters, residual analysis and test
+% for significance of the coefficents
+function [est_m,res,underfit_test,overfit_test] = arima_remarks(params,constant,vk)
 p = params(1);i=params(2);m=params(3);
 model = arima(p,i,m); 
+if constant == 0
+    model.Constant = constant;
+end
 est_m = estimate(model,vk);
 % Residual analysis
 % Residual computation
@@ -18,10 +26,19 @@ figure();
 parcorr(res,'NumLags',20)
 title('PACF of residuals from ARIMA(1,1,1) model')
 box off;
-% Whiteness test
-[h_model1,pval_model1] = lbqtest(res);
+% Whiteness test (test for underfit)
+[h_model,pval_model] = lbqtest(res);
 disp('Whiteness Test for Residuals results');
-disp(h_model1);disp(pval_model1);
-underfit = ~h_model1;
-summarize()
+disp(h_model);disp(pval_model);
+underfit_test = ~h_model;
+% uf = 1 means no underfit
+% Test for overfit
+summary = summarize(est_m);
+sig = (summary.Table.PValue <= 0.05) | isnan(summary.Table.PValue);
+overfit_test = prod(sig);
+% of = 1 means no overfit
+end
+%% Chow Test
+function y = chowt(x)
+    y = x;
 end
